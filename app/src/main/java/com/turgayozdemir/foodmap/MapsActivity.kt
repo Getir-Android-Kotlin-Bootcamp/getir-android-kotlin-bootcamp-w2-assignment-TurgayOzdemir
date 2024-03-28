a999fd1d136f82b4d758e29442a6b88af9c6f00f
Turgay Özdemir <ozdemir.turgay@outlook.com.tr>
2024-03-28 17:04:13 +0300

    Google Maps included.

---
+++ app/src/main/java/com/turgayozdemir/foodmap/MapsActivity.kt
@@ -0,0 +1,186 @@
+package com.turgayozdemir.foodmap
+
+import android.Manifest
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
+import com.turgayozdemir.foodmap.databinding.ActivityMapsBinding
+import java.io.IOException
+import java.util.Locale
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapLongClickListener{
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private lateinit var sharedPreferences: SharedPreferences
+    private var trackBoolean : Boolean? = null
+    private var selectedLatitude : Double? = null
+    private var selectedLongitude : Double? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        registerLauncher()
+
+        sharedPreferences = this.getSharedPreferences("com.turgayozdemir.travelbook", MODE_PRIVATE)
+        trackBoolean = false
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        try {
+            val success = mMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    this, R.raw.map_style
+                )
+            )
+            if (!success) {
+                Log.e("MapsActivity", "Harita stili ayarlama başarısız.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e("MapsActivity", "Harita stil dosyası bulunamadı.", e)
+        }
+
+        mMap.setOnMapLongClickListener(this)
+        locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
+
+        locationListener = object : LocationListener{
+            override fun onLocationChanged(location: Location) {
+                trackBoolean = sharedPreferences.getBoolean("trackBoolean", false)
+                if (trackBoolean == false){
+                    val userLocation = LatLng(location.latitude, location.longitude)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f))
+                    sharedPreferences.edit().putBoolean("trackBoolean", true).apply()
+                }
+
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(this@MapsActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this@MapsActivity, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Snackbar.make(binding.root, "Permission needed for location", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission") {
+                    permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                }.show()
+            } else{
+                permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        } else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f, locationListener)
+            val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (lastLocation != null){
+                val lastUserLocation = LatLng(lastLocation.latitude, lastLocation.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation, 15f))
+            }
+            mMap.addMarker(MarkerOptions().position(LatLng(lastLocation!!.latitude, lastLocation!!.longitude)))
+            val geocoder = Geocoder(this, Locale.getDefault())
+            try {
+                val addressList = geocoder.getFromLocation(lastLocation.latitude, lastLocation.longitude, 1)
+                if (addressList != null && addressList.size > 0) {
+                    val address = addressList[0]
+                    val addressStr = address.getAddressLine(0)
+                    binding.adress.text = addressStr
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            mMap.isMyLocationEnabled = false
+        }
+
+    }
+
+    private  fun registerLauncher(){
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it){
+                if (ContextCompat.checkSelfPermission(this@MapsActivity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,0f, locationListener)
+                    val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    if (lastLocation != null){
+                        val lastUserLocation = LatLng(lastLocation.latitude, lastLocation.longitude)
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation, 15f))
+                    }
+                }
+            }
+            else{
+                Toast.makeText(this@MapsActivity, "Permission needed!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onMapLongClick(p0: LatLng) {
+        mMap.clear()
+
+        mMap.addMarker(MarkerOptions().position(p0))
+        selectedLatitude = p0.latitude
+        selectedLongitude = p0.longitude
+
+        val geocoder = Geocoder(this, Locale.getDefault())
+        try {
+            val addressList = geocoder.getFromLocation(p0.latitude, p0.longitude, 1)
+            if (addressList != null && addressList.size > 0) {
+                val address = addressList[0]
+                val addressStr = address.getAddressLine(0)
+                binding.adress.text = addressStr
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun onSetLocation(view : View){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        if (lastLocation != null){
+            val lastUserLocation = LatLng(lastLocation.latitude, lastLocation.longitude)
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation, 15f))
+        }
+
+        val geocoder = Geocoder(this, Locale.getDefault())
+        try {
+            val addressList = geocoder.getFromLocation(lastLocation!!.latitude, lastLocation!!.longitude, 1)
+            if (addressList != null && addressList.size > 0) {
+                val address = addressList[0]
+                val addressStr = address.getAddressLine(0)
+                binding.adress.text = addressStr
+                mMap.addMarker(MarkerOptions().position(LatLng(lastLocation!!.latitude, lastLocation!!.longitude)))
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+}
